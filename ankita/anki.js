0f79ec16e0c84b89fd5540c1b0f06a7041c7a49f
ankita45 <shankita45@gmail.com>
2016-06-10 15:08:50 +0530

    hi

--- ankita/anki.js
+++ ankita/anki.js
@@ -9,7 +9,25 @@ function displayForm(res) {
         res.writeHead(200, {
             'Content-Type': 'text/html'
                 });
-        res.write(data);
+        res.write('<!DOCTYPE html>'+
+        	'<html>'+
+'<head>'+
+'</head>'+
+   '<body>'+
+      '<form action= method="post" >'+
+            '<label for="name">Name:</label>'+
+            '<input type="text"  placeholder="Enter your full name" />'+
+          
+            '<label for="email">Email:</label>'+
+            '<input type="email"  placeholder="Enter your email address" />'+
+            
+            '<label for="password">password:</label>'+
+            '<input type="password"  placeholder="Enter your password" />'+
+           
+            '<input type="submit" value="submit" />'+
+      '</form>'+
+   '</body>'+
+'</html>');
         res.end();
     });
 }
