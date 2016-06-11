8f64486904465f75a4ddc7e1e03052e3d9a36ba3
ankita45 <shankita45@gmail.com>
2016-06-11 11:10:22 +0530

    hi

---
+++ cb.js
@@ -0,0 +1,8 @@
+var sayHello=function(){
+	console.log("hello!");
+}
+var sayGoodbye=function(){
+	console.log('goodbye!');
+}
+exports.sayHello=sayHello;
+exports.sayGoodbye=sayGoodbye;
---
+++ mymodule.js
@@ -0,0 +1,3 @@
+var myModule=require('./cb.js');
+myModule.sayHello();
+myModule.sayGoodbye();
