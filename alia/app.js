3447e613dca6d595c2037fecb4367c31fb510f09
ankita45 <shankita45@gmail.com>
2016-06-15 11:49:23 +0530

    hello

---
+++ alia/app.js
@@ -0,0 +1,148 @@
+
+var express  = require('express'),
+   mongoose = require('mongoose'),
+   bodyParser = require('body-parser'),
+    ejs      = require('ejs')
+
+    // Mongoose Schema definition
+//Update user model (todo)
+    Schema = new mongoose.Schema({
+      firstName       : String,
+      name    : String,
+      department: String,
+      year    : String ,   
+      isPassedOut    : Boolean    
+    }),
+
+    User = mongoose.model('User', Schema);
+
+    mongoose.connect('mongodb://Ankita:1234@ds013564.mlab.com:13564/ankita-database');
+
+
+    var app = express()
+    
+    app.use(bodyParser.json()); // get information from html forms
+    app.use(bodyParser.urlencoded({extended: true}));
+ 
+
+  app.get('/api', function (req, res) {
+    res.json(200, {msg: 'OK' });
+  })
+
+app.get('/', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#query_Query-find
+    User.find({}, function ( err, users ){
+        if(!err && users){
+            res.render('users.ejs',{
+                data :  users
+            })
+        } else {
+            console.log(err)
+        }
+    });
+});
+
+app.get('/adduser', function(req, res){
+    res.render('addUser.ejs')
+})
+
+  app.post('/api/adduser', function (req, res) {
+
+      //update post function as per new model (todo)
+        var user = new User(
+        {
+        StudentId : req.body.StudentId,
+        name : req.body.name,
+        department : req.body.department,
+        year : req.body.year,
+        isPassedOut : req.body.isPassedOut
+        }
+    );
+  
+    // http://mongoosejs.com/docs/api.html#model_Model-save
+    user.save(function (err, data) {
+        if(!err && data){
+            console.log('Data added successfully');
+            res.redirect('/')
+        } else {
+            res.json(500, {msg: 'Something went wrong' });
+            console.log(err)
+        }
+      
+    });
+  })
+
+  app.get('/api/users', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#query_Query-remove
+    User.remove({ department: 'civil' }, function ( err ) {
+        if(!err){
+            console.log("User deleted successfully")
+        } else{
+            console.log(err)
+        }
+    });
+  })
+
+app.get('/userdetails/:id', function(req, res){
+    User.findById( req.params.id, function ( err, user ) {
+        if(!err && user){
+            res.render('userDetail.ejs',{
+                data : user
+            })
+        } else {
+            console.log(err)
+        }
+    });
+} )
+
+  // app.get('/api/users/:id', function (req, res) {
+  //   // http://mongoosejs.com/docs/api.html#model_Model.findById
+  //
+  // })
+
+
+app.get('/edituser/:id', function(req, res){
+    User.findById( req.params.id, function ( err, user ) {
+        if(!err && user){
+            res.render('editUser.ejs',{
+                data : user
+            })
+        } else {
+            console.log(err)
+        }
+    });
+
+});
+
+  app.post('/api/edituser/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    User.findById( req.params.id, function ( err, user ) {
+        console.log(user)
+        user.name = req.body.name
+       user.department = req.body.department;
+      // http://mongoosejs.com/docs/api.html#model_Model-save
+      user.save( function ( err, data ){
+          if(!err && data){
+          res.redirect('/')
+          } else {
+              console.log(err)
+          }
+
+      });
+    });
+  });
+
+  app.get('/api/deleteUser/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    User.findById( req.params.id, function ( err, user ) {
+      // http://mongoosejs.com/docs/api.html#model_Model.remove
+      user.remove( function ( err ){
+          console.log("User deleted successfully")
+          res.redirect('/')
+      });
+    });
+  })
+
+app.listen(1338);
+console.log('Magic happens on port 1338');
+
